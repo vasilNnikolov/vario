3b45578ea8abc6e841ad8fe2c1e39995cd00d415
Vasil Nikolov <vasil.nikolov@endurosat.com>
2024-05-08 20:36:29 +0300

    figured out defmt

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # Vario
 
 A variometer based on the [Raspberry Pi Pico](https://www.raspberrypi.com/documentation/microcontrollers/raspberry-pi-pico.html)
+
+## Software
+
+For debugging, see [this link](https://datasheets.raspberrypi.com/pico/getting-started-with-pico.pdf) specifically section `Debug with a second Pico`
--- software/open-vario/src/main.rs
+++ software/open-vario/src/main.rs
@@ -1,88 +1,29 @@
+//! This example test the RP Pico on board LED.
+//!
+//! It does not work with the RP Pico W board. See wifi_blinky.rs.
+
 #![no_std]
 #![no_main]
-use bme280;
+
 use defmt::*;
 use embassy_executor::Spawner;
-use embassy_rp::{gpio, i2c};
-use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
-use embassy_sync::mutex::Mutex;
-use embassy_time::Delay;
-use embassy_time::{Duration, Ticker, Timer};
-use embedded_hal::delay::DelayNs;
-
+use embassy_rp::gpio;
+use embassy_time::Timer;
 use gpio::{Level, Output};
 use {defmt_rtt as _, panic_probe as _};
 
-type LedType = Mutex<ThreadModeRawMutex, Option<Output<'static>>>;
-static LED: LedType = Mutex::new(None);
-
 #[embassy_executor::main]
-async fn main(spawner: Spawner) {
+async fn main(_spawner: Spawner) {
     let p = embassy_rp::init(Default::default());
-
-    // add led blinking
-    let led = Output::new(p.PIN_25, Level::High);
-    {
-        *(LED.lock().await) = Some(led);
-    }
-    let dt = 100 * 1_000_000;
-    let k = 1.003;
-
-    unwrap!(spawner.spawn(toggle_led(&LED, Duration::from_nanos(dt))));
-    unwrap!(spawner.spawn(toggle_led(
-        &LED,
-        Duration::from_nanos((dt as f64 * k) as u64)
-    )));
-
-    Timer::after(Duration::from_millis(10_000)).await;
-
-    // init the bme280 i2c
-    let (sda, sck) = (p.PIN_0, p.PIN_1);
-    let pressure_i2c = i2c::I2c::new_blocking(p.I2C0, sck, sda, i2c::Config::default());
-
-    let mut bme = bme280::i2c::BME280::new_primary(pressure_i2c);
-    let mut delay = Delay {};
-    // delay.delay_ms(10000);
-
-    match bme.init(&mut delay) {
-        Ok(_) => {
-            info!("bme init success");
-            let mut led = LED.lock().await;
-            if let Some(pin) = led.as_mut() {
-                for _ in 0..10 {
-                    pin.toggle();
-                    delay.delay_ms(100);
-                }
-            }
-        }
-        Err(_) => {
-            warn!("bme init fail");
-            let mut led = LED.lock().await;
-            if let Some(pin) = led.as_mut() {
-                for i in 0..20 {
-                    pin.toggle();
-                    delay.delay_ms(i * 100);
-                }
-            }
-            crate::panic!()
-        }
-    }
+    let mut led = Output::new(p.PIN_25, Level::Low);
 
     loop {
-        let _p = bme.measure(&mut delay).unwrap().pressure;
-    }
-}
+        info!("led on!");
+        led.set_high();
+        Timer::after_millis(100).await;
 
-#[embassy_executor::task(pool_size = 2)]
-async fn toggle_led(led: &'static LedType, delay: Duration) {
-    let mut ticker = Ticker::every(delay);
-    loop {
-        {
-            let mut led_unlocked = led.lock().await;
-            if let Some(pin_ref) = led_unlocked.as_mut() {
-                pin_ref.toggle();
-            }
-        }
-        ticker.next().await;
+        info!("led off!");
+        led.set_low();
+        Timer::after_secs(1).await;
     }
 }
