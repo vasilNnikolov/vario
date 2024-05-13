504d0b2f5e3e4e068eafb23e3a6c811ffe0c0c39
Vasil Nikolov <vasil.nikolov@endurosat.com>
2024-05-13 20:12:48 +0300

    add h, v computation

--- software/open-vario/src/main.rs
+++ software/open-vario/src/main.rs
@@ -12,12 +12,16 @@ use embassy_rp::gpio;
 use embassy_rp::i2c::{self, Config};
 use embassy_sync::blocking_mutex::raw::ThreadModeRawMutex;
 use embassy_sync::mutex::Mutex;
-use embassy_time::{Delay, Timer};
+use embassy_time::{Delay, Duration, Timer};
 use gpio::{Level, Output};
 use {defmt_rtt as _, panic_probe as _};
 
 type LedType = Mutex<ThreadModeRawMutex, Option<Output<'static>>>;
 static LED: LedType = Mutex::new(None);
+const G: f32 = 9.81; // m.s^-2
+const MU: f32 = 29e-3; // kg.mol^-1
+const R: f32 = 8.314; // J.mol^-1
+const DT: Duration = Duration::from_millis(100);
 
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
@@ -34,7 +38,7 @@ async fn main(spawner: Spawner) {
     let scl = p.PIN_15;
 
     info!("set up i2c ");
-    let mut i2c = i2c::I2c::new_blocking(p.I2C1, scl, sda, Config::default());
+    let i2c = i2c::I2c::new_blocking(p.I2C1, scl, sda, Config::default());
 
     let mut ps = bme280::i2c::BME280::new_primary(i2c);
     let mut d = Delay {};
@@ -47,20 +51,38 @@ async fn main(spawner: Spawner) {
             error!("BME init failed");
         }
     }
-    info!("End of program");
+    let mut last_p: Option<f32> = None;
+    let mut h: f32 = 0.;
 
     loop {
         match ps.measure(&mut d) {
             Ok(data) => {
                 let p = data.pressure;
+                if let None = last_p {
+                    last_p = Some(p);
+                }
                 let t = data.temperature;
                 info!("{} Pa, {} C", p, t);
+
+                if let Some(last_p) = last_p {
+                    let dpdt = (p - last_p) / (DT.as_millis() as f32 / 1000.0);
+
+                    let v = -(R * (t + 273.15)) / (G * p * MU) * dpdt;
+                    h += v * (DT.as_millis() as f32 / 1000.);
+
+                    info!(
+                        "Vertical speed {} cm/s, height {} cm",
+                        100. * ((1000.0 * v) as i32) as f32 / 1000.0,
+                        100. * ((1000.0 * h) as i32) as f32 / 1000.0,
+                    );
+                }
+                last_p = Some(p);
             }
             Err(_) => {
                 error!("Could not measure from BME280");
             }
         }
-        Timer::after_millis(1000).await;
+        Timer::after(DT).await;
     }
 }
 
