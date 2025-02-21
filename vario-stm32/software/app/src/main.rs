a1d329f55eed6553086a5959133beda440e53bc1
Vasil Nikolov <vaskonikolov2003@gmail.com>
2025-02-21 20:46:57 +0200

    add defmt as dependency

--- vario-stm32/software/app/src/main.rs
+++ vario-stm32/software/app/src/main.rs
@@ -9,11 +9,16 @@ use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch
 
 use cortex_m::asm;
 use cortex_m_rt::entry;
+use defmt::info;
+use defmt_rtt as _;
 use stm32l0::stm32l0x1;
 
 #[entry]
 fn main() -> ! {
-    let _p = stm32l0x1::Peripherals::take().unwrap();
+    let core_p = cortex_m::Peripherals::take().unwrap();
+    let p = stm32l0x1::Peripherals::take().unwrap();
+    info!("abcd");
+
     asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
 
     loop {
