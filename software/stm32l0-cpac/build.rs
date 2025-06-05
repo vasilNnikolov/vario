7a9111d05475f078cd4a51d2b34b70bb4895b251
Vasil Nikolov <vaskonikolov2003@gmail.com>
2025-06-05 20:46:14 +0300

    c bindings in OUT_DIR

--- software/stm32l0-cpac/build.rs
+++ software/stm32l0-cpac/build.rs
@@ -1,20 +1,25 @@
+use std::env;
+use std::path::PathBuf;
+
 fn main() {
     println!("cargo:rerun-if-changed=./build.rs");
     println!("cargo:rerun-if-changed=./STM32CubeL0");
+
     let bindings = bindgen::Builder::default()
         .use_core()
         .clang_macro_fallback()
         .derive_default(true)
         .clang_arg("-I./STM32CubeL0/Drivers/CMSIS/Include")
         .header("./STM32CubeL0/Drivers/CMSIS/Device/ST/STM32L0xx/Include/stm32l072xx.h")
-        .raw_line("#![allow(non_snake_case)]") // add these to avoid rust warnings
-        .raw_line("#![allow(non_camel_case_types)]")
-        .raw_line("#![allow(non_upper_case_globals)]")
-        .raw_line("#![allow(unsafe_op_in_unsafe_fn)]")
+        // .raw_line("#![allow(non_snake_case)]") // add these to avoid rust warnings
+        // .raw_line("#![allow(non_camel_case_types)]")
+        // .raw_line("#![allow(non_upper_case_globals)]")
+        // .raw_line("#![allow(unsafe_op_in_unsafe_fn)]")
         .generate_comments(true)
         .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
         .generate()
         .expect("Unable to generate bindings");
 
-    bindings.write_to_file("src/c_bindings.rs").unwrap();
+    let out = &PathBuf::from(env::var_os("OUT_DIR").unwrap());
+    bindings.write_to_file(out.join("c_bindings.rs")).unwrap();
 }
