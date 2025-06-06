863eb948736b8f801c709053077132273d12dc51
Vasil Nikolov <vaskonikolov2003@gmail.com>
2025-06-06 20:55:54 +0300

    better macro

--- software/cpac-macros/src/lib.rs
+++ software/cpac-macros/src/lib.rs
@@ -1,4 +1,5 @@
-extern crate proc_macro;
+use std::any::Any;
+
 use proc_macro::TokenStream;
 use quote::quote;
 use syn::{
@@ -8,8 +9,13 @@ use syn::{
 };
 
 struct MacroInput {
+    /// path to the generated bindings file, inside "OUT_DIR". If the file is generated in env!("OUT_DIR")/a.rs, this should be "a.rs"
     file_name: LitStr,
+    /// name of the struct you want to find
     struct_name: Ident,
+    // /// name of the <STRUCT>_BASE u32 constant that shows the location of the peripheral
+    // base_name: Ident,
+    /// name of the generated module
     module_name: Ident,
 }
 
@@ -23,10 +29,13 @@ impl Parse for MacroInput {
             .parse()
             .inspect_err(|_| eprintln!("Could not parse struct_name out of macro input"))?;
         input.parse::<Token![,]>()?;
+        // let base_name: Ident = input.parse()?;
+        // input.parse::<Token![,]>()?;
         let module_name: Ident = input.parse()?;
         Ok(MacroInput {
             file_name,
             struct_name,
+            // base_name,
             module_name,
         })
     }
@@ -37,32 +46,46 @@ pub fn find_struct(input: TokenStream) -> TokenStream {
     let MacroInput {
         file_name,
         struct_name,
+        // base_name,
         module_name,
     } = parse_macro_input!(input as MacroInput);
 
-    // Read the file contents
     let file_path = std::path::Path::new(&std::env::var("OUT_DIR").expect("OUT_DIR not set"))
         .join(file_name.value());
     let file_content = std::fs::read_to_string(&file_path)
         .unwrap_or_else(|_| panic!("Unable to read file {}", file_path.display()));
 
-    // Parse file content as a Rust AST
     let syntax_tree = syn::parse_file(&file_content).expect("Unable to parse file content");
 
-    // Find the struct with the specified name
-    for item in syntax_tree.items {
-        if let syn::Item::Struct(ref struct_item) = item {
-            if struct_item.ident == struct_name {
-                // Return the struct definition as a TokenStream
-                return TokenStream::from(quote! {
-                    pub mod #module_name {
-                        #struct_item
-                        pub const MODCONST: u8=1;
-                    }
-                });
-            }
-        }
-    }
+    // get structs whose name matches struct_name
+    let structs_with_matching_name: Vec<_> = syntax_tree
+        .items
+        .iter()
+        .filter(|&item| match item {
+            syn::Item::Struct(s) => s.ident == struct_name,
+            _ => false,
+        })
+        .collect();
 
-    panic!("Struct {} not found in {}", struct_name, file_name.value());
+    assert!(
+        structs_with_matching_name.len() == 1,
+        "There are multiple structs whose ident is {struct_name}"
+    );
+    let struct_item = structs_with_matching_name[0];
+
+    TokenStream::from(quote! {
+        pub mod #module_name {
+            #struct_item
+
+            impl #struct_name {
+                pub fn new_static_ref() -> &'static mut Self {
+                    unsafe {
+                        let ptr = 0x1234 as *mut Self;
+                        &mut *ptr
+                    }
+                }
+            }
+            pub const MODCONST: u8=1;
+        }
+    })
 }
