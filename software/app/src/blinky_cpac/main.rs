aea744cb965d41f3e21fac45bfae7d77875ed7aa
Vasil Nikolov <vaskonikolov2003@gmail.com>
2025-06-16 19:16:25 +0300

    switched to HSE, somehow it's slower than expected

--- software/app/src/blinky_cpac/main.rs
+++ software/app/src/blinky_cpac/main.rs
@@ -13,17 +13,41 @@ use stm32l0_cpac as cpac;
 
 struct BusyLoopDelayNs;
 
+const CPU_FREQ: u32 = 16_000_000;
 impl embedded_hal::delay::DelayNs for BusyLoopDelayNs {
     fn delay_ns(&mut self, ns: u32) {
-        const CPU_FREQ: u32 = 2_000_000;
         let d_cycles = ns as u64 * CPU_FREQ as u64 / 1_000_000_000 as u64;
         cortex_m::asm::delay(d_cycles as u32);
     }
 }
 
+fn init_HSE() {
+    let rcc = rcc::RCC_TypeDef::new_static_ref();
+    modify_field(&mut rcc.CR, rcc::CR_HSEON_Msk, 1);
+    loop {
+        info!("Getting HSE status");
+        let is_hse_on = (rcc.CR.read() & rcc::CR_HSERDY_Msk) >> rcc::CR_HSERDY_Pos;
+        if is_hse_on == 1 {
+            info!("HSE ready");
+            break;
+        }
+        info!("HSE not ready")
+    }
+    modify_field(&mut rcc.CFGR, rcc::CFGR_SW, 0b10);
+    loop {
+        let hse_switch_status = (rcc.CFGR.read() & rcc::CFGR_SWS) >> rcc::CFGR_SWS_Pos;
+        info!("status of HSE switch: {}", hse_switch_status);
+        if hse_switch_status == 0b10 {
+            break;
+        }
+    }
+    info!("switched to HSE");
+}
+
 #[entry]
 fn main() -> ! {
     info!("Start");
+    init_HSE();
     let rcc = rcc::RCC_TypeDef::new_static_ref();
     modify_field(&mut rcc.IOPENR, rcc::IOPENR_IOPBEN_Msk, 1);
 
@@ -46,7 +70,7 @@ fn main() -> ! {
 
         // turn PB12 off
         modify_field(&mut gpio_b.BSRR, gpio_b::BSRR_BR_12, 1);
-        bld.delay_ms(1000);
+        bld.delay_ms(100);
         i += 1;
     }
 }
