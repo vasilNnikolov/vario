92c72bfeda23f88f5aec13ea28a2d78482e14a8f
Vasil Nikolov <vaskonikolov2003@gmail.com>
2025-06-16 18:48:57 +0300

    add simpler modify_field function, ZCA to modify_register

--- software/app/src/blinky_cpac/main.rs
+++ software/app/src/blinky_cpac/main.rs
@@ -8,7 +8,7 @@ use bsp as _; // do not remove, the stm32l0 crate is needed for compilation and 
 use cortex_m_rt::entry;
 use defmt::info;
 
-use cpac::{gpio_b, modify_reg, rcc};
+use cpac::{gpio_b, modify_field, rcc};
 use stm32l0_cpac as cpac;
 
 struct BusyLoopDelayNs;
@@ -25,42 +25,27 @@ impl embedded_hal::delay::DelayNs for BusyLoopDelayNs {
 fn main() -> ! {
     info!("Start");
     let rcc = rcc::RCC_TypeDef::new_static_ref();
-    modify_reg(
-        &mut rcc.IOPENR,
-        rcc::IOPENR_IOPBEN_Msk,
-        rcc::IOPENR_IOPBEN_Pos,
-        1,
-    );
+    modify_field(&mut rcc.IOPENR, rcc::IOPENR_IOPBEN_Msk, 1);
 
     let gpio_b = cpac::gpio_b::GPIO_TypeDef::new_static_ref();
     // set output mode
-    modify_reg(
-        &mut gpio_b.MODER,
-        gpio_b::MODER_MODE12,
-        gpio_b::MODER_MODE12_Pos,
-        0b01,
-    );
+    modify_field(&mut gpio_b.MODER, gpio_b::MODER_MODE12, 0b01);
     // set output type to push-pull
-    modify_reg(&mut gpio_b.OTYPER, gpio_b::OTYPER_OT_12, 12, 0b0);
+    modify_field(&mut gpio_b.OTYPER, gpio_b::OTYPER_OT_12, 0b0);
 
     // set no pull-up & no pull-down
-    modify_reg(
-        &mut gpio_b.PUPDR,
-        gpio_b::PUPDR_PUPD12,
-        gpio_b::PUPDR_PUPD12_Pos,
-        0b00,
-    );
+    modify_field(&mut gpio_b.PUPDR, gpio_b::PUPDR_PUPD12, 0b00);
 
     let mut bld = BusyLoopDelayNs;
     let mut i = 0;
     loop {
         info!("Counter: {}", i);
         // turn PB12 on
-        modify_reg(&mut gpio_b.BSRR, gpio_b::BSRR_BS_12, 12, 1);
+        modify_field(&mut gpio_b.BSRR, gpio_b::BSRR_BS_12, 1);
         bld.delay_ms(100);
 
         // turn PB12 off
-        modify_reg(&mut gpio_b.BSRR, gpio_b::BSRR_BR_12, 28, 1);
+        modify_field(&mut gpio_b.BSRR, gpio_b::BSRR_BR_12, 1);
         bld.delay_ms(1000);
         i += 1;
     }
--- software/stm32l0-cpac/src/lib.rs
+++ software/stm32l0-cpac/src/lib.rs
@@ -28,3 +28,23 @@ pub fn modify_reg(reg: &mut volatile_register::RW<u32>, clear_mask: u32, pos: u3
 
     unsafe { reg.modify(|x| (x & (!clear_mask)) | (value << pos)) }
 }
+
+/// modifies a register field.
+/// `field_mask` is a mask that shows the field (ex 0x00000010)
+/// `value`: what you want written to the register.
+///
+/// if the `safe-modify-reg` feature is enabled, the function will check if `value` is fully inside `clear_mask`, and if not, will panic
+#[inline(always)]
+pub fn modify_field(reg: &mut volatile_register::RW<u32>, field_mask: u32, value: u32) {
+    let pos = field_mask.trailing_zeros(); // zero-cost abstraction if clear_mask is a constant
+
+    #[cfg(feature = "safe-modify-reg")]
+    if (value << pos) & (!field_mask) != 0 {
+        panic!(
+            "tried to modify a register with clear_mask: {}, position: {}, value: {}",
+            field_mask, pos, value
+        );
+    }
+
+    unsafe { reg.modify(|x| (x & (!field_mask)) | (value << pos)) }
+}
